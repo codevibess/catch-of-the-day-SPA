572f05eb763f704fead2824f7bd701132ffb07cd
Andriy Cherevko <cherevkin@gmail.com>
2018-05-11 13:18:20 +0200

    Handling events, Routing to the typed store

--- src/components/Router.js
+++ src/components/Router.js
@@ -7,12 +7,12 @@ import NotFound from './NotFound';
 
 const Router = () => (
     <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={StorePicker}> </Route>
-            <Route  path="/store/:storeId" component={App}> </Route>
-            <Route  component={NotFound}> </Route>
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
-)
+  );
 
 export default Router;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,27 @@ import React from 'react';
 import {getFunName} from '../helpers'
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+    constructor(){
+        super();
+        this.goToStore = this.goToStore.bind(this);
+        console.log('commm')
+    }
+    goToStore(event){
+        //stop thee form from submiting
+        event.preventDefault();
+        //get the text from that input
+
+       const storeName =  this.myInput.value.value;
+
+        //Change the page to /store/whatever
+        this.props.history.push(`/store/${storeName}`);
+    }
     render() {
         return (
-            <form action="" className="store-selector">
+            <form action="" className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/>
                 <button type="submit">Visit Store</button>
             </form>
         )
