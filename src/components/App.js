fe243690f07765e3d0e81603988335eaf2a4cc18
Andriy Cherevko <cherevkin@gmail.com>
2018-05-22 12:24:04 +0200

    Persist State to firebase, mount and unmount methods

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,25 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
-  
   state = {
     fishes: {},
     order: {}
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
+
+  componentWillUnmount(){
+      base.removeBinding(this.ref);
+  }
 
   addFish = fish => {
     //Take a opy of existing state
@@ -53,7 +65,7 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
